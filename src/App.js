6d112c07343372ceda91c425578d14eff3066d7c
Paul0-Henrique-Da-Silva <ph46163835@gmail.com>
2022-06-03 16:15:15 -0300

    tryunfo-form

--- src/App.js
+++ src/App.js
@@ -3,34 +3,56 @@ import Form from './components/Form';
 import Card from './components/Card';
 
 class App extends React.Component {
-constructor(props){
- super(props);
-  this.state = {
-    cardName: '',
-    cardDescription: '',
-    cardAttr1: '0',
-    cardAttr3: '0',
-    cardAttr2: '0',
-    cardImage: '',
-    cardRare: '',
-    cardTrunfo: false,
-    isSaveButtonDisabled: true,
-    deck: [],
-    baralho: [],
+  constructor(props) {
+    super(props);
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr3: '0',
+      cardAttr2: '0',
+      cardImage: '',
+      cardRare: '',
+      card: [],
+      cardTrunfo: false,
+      isSaveButtonDisabled: true,
+    };
+    this.inChange = this.inChange.bind(this);
+    this.cleanStade = this.cleanStade.bind(this);
+    this.validateTrunfo = this.validateTrunfo.bind(this);
   }
-  this.inChange = this.inChange.bind(this);
-}
+
+  validateTrunfo() {
+    this.setState(() => ({
+      hasTrunfo: true,
+    }));
+  }
+
+  cleanStade() {
+    this.setState((prevState) => ({
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '0',
+      cardAttr2: '0',
+      cardAttr3: '0',
+      cardImage: '',
+      cardRare: '',
+      cardTrunfo: false,
+      card: [...prevState.card, prevState],
+    }));
+    this.validateTrunfo();
+  }
 
   inChange(event) {
-      const { target } = event;
-      const value = target.type === 'checkbox' ? target.checked : target.value;
-      const { name } = target; 
+    const { target } = event;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name } = target;
     this.setState({
       [name]: value,
-     }, () => this.isButtonImput());
+    }, () => this.isButtonImput());
   }
 
-  isButtonImput (){
+  isButtonImput() {
     const MIN_ONLY_VALUE = 0;
     const MAX_ONLY_VALUE = 90;
     const MAX_VALUE_SUM = 210;
@@ -38,7 +60,7 @@ constructor(props){
       cardImage } = this.state;
     const attr1 = Number(cardAttr1);
     const attr2 = Number(cardAttr2);
-    const attr3 = Number(cardAttr3);  
+    const attr3 = Number(cardAttr3);
     const SUM_VALUE = attr1 + attr2 + attr3;
     if (cardName === ''
       || cardImage === ''
@@ -50,35 +72,28 @@ constructor(props){
       || attr3 > MAX_ONLY_VALUE
       || attr3 < MIN_ONLY_VALUE
       || SUM_VALUE > MAX_VALUE_SUM
-      )
-       {
+    ) {
       return this.setState({
         isSaveButtonDisabled: true,
       });
-    } 
-   return this.setState({
-     isSaveButtonDisabled: false,
-   }) 
+    }
+    return this.setState({
+      isSaveButtonDisabled: false,
+    });
   }
 
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
-      cardImage, cardRare, cardTrunfo, isSaveButtonDisabled } = this.state;
+      cardImage, cardRare, cardTrunfo, isSaveButtonDisabled, card } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form 
-         cardName={cardName}
-         onInputChange={ this.inChange }
-         cardDescription={ cardDescription }
-         cardAttr1={ cardAttr1 }
-         cardAttr2={ cardAttr2 }
-         cardAttr3={ cardAttr3 }
-         cardImage={ cardImage }
-         cardRare={ cardRare }
-         cardTrunfo={ cardTrunfo }
-         isSaveButtonDisabled={ isSaveButtonDisabled } 
-         />
+        <Form
+          { ...this.state }
+          onSaveButtonClick={ this.cleanStade }
+          onInputChange={ this.inChange }
+          isSaveButtonDisabled={ isSaveButtonDisabled }
+        />
         <Card
           cardName={ cardName }
           cardDescription={ cardDescription }
@@ -89,6 +104,21 @@ constructor(props){
           cardRare={ cardRare }
           cardTrunfo={ cardTrunfo }
         />
+        {
+          card.map((cardCurrent) => (
+            <Card
+              key={ cardCurrent.cardName }
+              cardName={ cardCurrent.cardName }
+              cardDescription={ cardCurrent.cardDescription }
+              cardAttr1={ cardCurrent.cardAttr1 }
+              cardAttr2={ cardCurrent.cardAttr2 }
+              cardAttr3={ cardCurrent.cardAttr3 }
+              cardImage={ cardCurrent.cardImage }
+              cardRare={ cardCurrent.cardRare }
+              cardTrunfo={ cardCurrent.cardTrunfo }
+            />
+          ))
+        }
       </div>
     );
   }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -5,72 +5,72 @@ class Form extends React.Component {
   render() {
     const { cardName, cardDescription, cardAttr1, cardAttr2, cardAttr3,
       cardImage, cardRare, cardTrunfo, isSaveButtonDisabled,
-      onInputChange, onSaveButtonClick } = this.props;
+      onInputChange, onSaveButtonClick, hasTrunfo } = this.props;
     return (
-        // https://react-bootstrap.github.io/forms/floating-labels/
+    // https://react-bootstrap.github.io/forms/floating-labels/
       <form>
-          <input
-            name="cardName"
-            label="Nome"
-            id="name"
-            type="text"
-            data-testid="name-input"
-            value={ cardName }
-            onChange={ onInputChange }
-          />
-        
-          <textarea
-            label="Descriçao"
-            placeholder="Descrição"
-            name="cardDescription"
-            id="descri"
-            data-testid="description-input"
-            value={ cardDescription }
-            onChange={ onInputChange }
-          />
+        <input
+          name="cardName"
+          label="Nome"
+          id="name"
+          type="text"
+          data-testid="name-input"
+          value={ cardName }
+          onChange={ onInputChange }
+        />
 
-          <input
-            label="attr01"
-            placeholder="attr01"
-            name="cardAttr1"
-            id="attr01"
-            type="number"
-            data-testid="attr1-input"
-            value={ cardAttr1 }
-            onChange={ onInputChange }
-          />
+        <textarea
+          label="Descriçao"
+          placeholder="Descrição"
+          name="cardDescription"
+          id="descri"
+          data-testid="description-input"
+          value={ cardDescription }
+          onChange={ onInputChange }
+        />
 
-          <input
-            label="attr02"
-            placeholder="attr02"
-            name="cardAttr2"
-            id="attr02"
-            type="number"
-            data-testid="attr2-input"
-            value={ cardAttr2 }
-            onChange={ onInputChange }
-          />
+        <input
+          label="attr01"
+          placeholder="attr01"
+          name="cardAttr1"
+          id="attr01"
+          type="number"
+          data-testid="attr1-input"
+          value={ cardAttr1 }
+          onChange={ onInputChange }
+        />
 
-          <input
-            label="attr03"
-            placeholder="attr03"
-            name="cardAttr3"
-            id="attr03"
-            type="number"
-            data-testid="attr3-input"
-            value={ cardAttr3 }
-            onChange={ onInputChange }
-          />
+        <input
+          label="attr02"
+          placeholder="attr02"
+          name="cardAttr2"
+          id="attr02"
+          type="number"
+          data-testid="attr2-input"
+          value={ cardAttr2 }
+          onChange={ onInputChange }
+        />
 
-          <input
-            label="Img-input"
-            placeholder="Url da Imagem"
-            name="cardImage"
-            id="img"
-            data-testid="image-input"
-            value={ cardImage }
-            onChange={ onInputChange }
-          />
+        <input
+          label="attr03"
+          placeholder="attr03"
+          name="cardAttr3"
+          id="attr03"
+          type="number"
+          data-testid="attr3-input"
+          value={ cardAttr3 }
+          onChange={ onInputChange }
+        />
+
+        <input
+          label="Img-input"
+          placeholder="Url da Imagem"
+          name="cardImage"
+          id="img"
+          data-testid="image-input"
+          value={ cardImage }
+          onChange={ onInputChange }
+        />
 
         <label htmlFor="type-rare">
           Raridade
@@ -87,25 +87,26 @@ class Form extends React.Component {
           </select>
         </label>
 
-        <label htmlFor="check">
-          <input
-            name="cardTrunfo"
-            id="check"
-            type="checkbox"
-            data-testid="trunfo-input"
-            checked={ cardTrunfo }
-            onChange={ onInputChange }
-          />
-          Super Trybe Trunfo
-        </label>
+        {hasTrunfo === true ? <p>Você já tem um Super Trunfo em seu baralho</p>
+          : (
+            <label htmlFor="check">
+              <input
+                name="cardTrunfo"
+                id="check"
+                type="checkbox"
+                data-testid="trunfo-input"
+                checked={ cardTrunfo }
+                onChange={ onInputChange }
+              />
+              Super Trybe Trunfo
+            </label>)}
 
         <button
-          onClick={ onSaveButtonClick }
           name="isSaveButtonDisabled"
           type="submit"
           data-testid="save-button"
           disabled={ isSaveButtonDisabled }
-          
+          onClick={ onSaveButtonClick }
         >
           Salvar
         </button>
@@ -128,4 +129,5 @@ Form.propTypes = {
   isSaveButtonDisabled: PropTypes.bool.isRequired,
   onInputChange: PropTypes.func.isRequired,
   onSaveButtonClick: PropTypes.func.isRequired,
+  hasTrunfo: PropTypes.bool.isRequired,
 };
